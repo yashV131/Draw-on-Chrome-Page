661f598fc7d5f4a67c81bb79068a0f5072bb2703
Yashvi <147964547+yashV131@users.noreply.github.com>
2025-12-23 12:04:45 -0600

    Added a readme and changed popup.js

--- popup.js
+++ popup.js
@@ -1,6 +1,25 @@
-// document.getElementById("toggle").addEventListener("click", async (e) => {
-//     const [tab] = await chrome.tabs.query({
-//         active: true,
-//         currentWindow: true
-//     })
-// });
+const toggleBtn = document.getElementById('toggle');
+
+let isToggled = false;
+
+toggleBtn.addEventListener('click', () => {
+    isToggled = !isToggled;
+    if (isToggled) {
+        document.body.style.backgroundColor = 'red';
+        toggleBtn.textContent = 'Stop drawing';
+    } else {
+        document.body.style.backgroundColor = 'green';
+        toggleBtn.textContent = 'Start drawing';
+    }
+});
+
+document.getElementById("toggle").addEventListener("click", async () => {
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  });
+
+  chrome.tabs.sendMessage(tab.id, {
+    action: "toggleDrawing"
+  });
+});
---
+++ readMe.txt
@@ -0,0 +1,12 @@
+Everythin I want:
+
+1. Be able to chose colors and draw them out on the page
+2. Ctrl + Z = undo
+3. Ctrl + Y = redo
+4. b = toggle color to blue
+5. y = toggle color to yellow
+6. r = toggle color to red
+7. p = open a window that lets one add pictures on a page.
+8. Ctrl + P = Print
+
+
